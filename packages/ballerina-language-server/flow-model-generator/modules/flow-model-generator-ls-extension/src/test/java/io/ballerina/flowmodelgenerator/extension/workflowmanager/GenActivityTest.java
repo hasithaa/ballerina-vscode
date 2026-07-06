dd242ea5a11ec3a375dfb87501fc6f84d237c57a
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2026-07-06 11:27:08 +0530

    Add LS tests for workflowManager/genActivity
    Covers generating activities from a remote action with parameters and
    documentation, a remote action without description/parameters, and a resource
    action with a path parameter.

---
+++ packages/ballerina-language-server/flow-model-generator/modules/flow-model-generator-ls-extension/src/test/java/io/ballerina/flowmodelgenerator/extension/workflowmanager/GenActivityTest.java
@@ -0,0 +1,141 @@
+/*
+ *  Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com)
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package io.ballerina.flowmodelgenerator.extension.workflowmanager;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import io.ballerina.flowmodelgenerator.extension.request.GenActivityRequest;
+import io.ballerina.modelgenerator.commons.AbstractLSTest;
+import org.eclipse.lsp4j.TextEdit;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Tests for the WorkflowManagerService genActivity API.
+ *
+ * @since 1.5.0
+ */
+public class GenActivityTest extends AbstractLSTest {
+
+    private static final Type textEditListType = new TypeToken<Map<String, List<TextEdit>>>() {
+    }.getType();
+
+    @DataProvider(name = "data-provider")
+    @Override
+    protected Object[] getConfigsList() {
+        return new Object[][]{
+                {Path.of("gen_activity_remote_action.json")},
+                {Path.of("gen_activity_remote_action_no_desc.json")},
+                {Path.of("gen_activity_resource_action.json")}
+        };
+    }
+
+    @Override
+    @Test(dataProvider = "data-provider")
+    public void test(Path config) throws IOException {
+        Path configJsonPath = configDir.resolve(config);
+        TestConfig testConfig = gson.fromJson(Files.newBufferedReader(configJsonPath), TestConfig.class);
+
+        String filePath = sourceDir.resolve(testConfig.source()).toAbsolutePath().toString();
+        GenActivityRequest request = new GenActivityRequest(filePath, testConfig.diagram(),
+                testConfig.activityName(), testConfig.activityParameters(), testConfig.activityDescription(),
+                testConfig.connection());
+        JsonObject jsonMap = getResponseAndCloseFile(request, testConfig.source()).getAsJsonObject("textEdits");
+
+        Map<String, List<TextEdit>> actualTextEdits = gson.fromJson(jsonMap, textEditListType);
+
+        boolean assertFailure = actualTextEdits.size() != testConfig.output().size();
+
+        Map<String, List<TextEdit>> newMap = new HashMap<>();
+        for (Map.Entry<String, List<TextEdit>> entry : actualTextEdits.entrySet()) {
+            Path fullPath = Paths.get(entry.getKey());
+            String relativePath = sourceDir.relativize(fullPath).toString();
+
+            List<TextEdit> textEdits = testConfig.output().get(relativePath.replace("\\", "/"));
+            if (textEdits == null) {
+                log.info("No text edits found for the file: " + relativePath);
+                assertFailure = true;
+            } else if (!assertArray("text edits", entry.getValue(), textEdits)) {
+                assertFailure = true;
+            }
+
+            newMap.put(relativePath, entry.getValue());
+        }
+
+        if (assertFailure) {
+            TestConfig updatedConfig = new TestConfig(testConfig.source(), testConfig.description(),
+                    testConfig.activityName(), testConfig.activityDescription(), testConfig.activityParameters(),
+                    testConfig.connection(), testConfig.diagram(), newMap);
+//            updateConfig(configJsonPath, updatedConfig);
+            Assert.fail(String.format("Failed test: '%s' (%s)", testConfig.description(), configJsonPath));
+        }
+    }
+
+    @Override
+    protected String getResourceDir() {
+        return "workflow_manager";
+    }
+
+    @Override
+    protected Class<? extends AbstractLSTest> clazz() {
+        return GenActivityTest.class;
+    }
+
+    @Override
+    protected String getApiName() {
+        return "genActivity";
+    }
+
+    @Override
+    protected String getServiceName() {
+        return "workflowManager";
+    }
+
+    /**
+     * Represents the test configuration for the genActivity API.
+     *
+     * @param source              The source file path
+     * @param description         The description of the test
+     * @param activityName        The name of the activity function
+     * @param activityDescription The description of the activity
+     * @param activityParameters  The parameters of the activity function
+     * @param connection          The name of the connection
+     * @param diagram             The action call flow node
+     * @param output              The expected text edits
+     */
+    private record TestConfig(String source, String description, String activityName, String activityDescription,
+                              JsonElement activityParameters, String connection, JsonElement diagram,
+                              Map<String, List<TextEdit>> output) {
+
+        public String description() {
+            return description == null ? "" : description;
+        }
+    }
+}
